ce9d2fca3aa8ecb5a5e4f4bbb8f1eead89cf7456
thchan096 <dangthithuytrang321@gmail.com>
2024-09-26 21:05:57 +0700

    bai34.py

--- bai34.py
+++ bai34.py
@@ -5,20 +5,16 @@ Created on Mon Sep  2 16:52:54 2024
 @author: ADMINIS
 """
 
-N = int(input("Nhập số nguyên dương N: "))
-while N <= 0:
-    N = int(input("Nhập lại số nguyên dương N: "))
-if N == 1:
-    print("1 không phải là số nguyên tố")
-elif N == 2:
-    print("2 là số nguyên tố")
+n = int(input("Nhập n: "))
+while n<=0:
+    n = int(input("Nhập lại n "))
+if n<2:
+    print("Không phải là số nguyên tố")
 else:
-    x = False
-    for i in range(2, N-1):
-        if N%i == 0:
-           x = True
-           break
-    if x:
-           print(f"{N} không phải là số nguyên tố")
-    else:
-       print(f"{N} là số nguyên tố")
+    for i in range(2,n+1):
+        if n%i==0:
+            print("Không phải là số nguyên tố")
+            break
+        else:
+            print("Là số nguyên tố") 
+            break
